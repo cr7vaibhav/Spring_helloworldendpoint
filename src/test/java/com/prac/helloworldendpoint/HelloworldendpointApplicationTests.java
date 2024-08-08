4b50676b313a933fe6ee4dbcb4e628ac955096a1
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-08-08 16:54:14 +0530

    first

---
+++ src/test/java/com/prac/helloworldendpoint/HelloworldendpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prac.helloworldendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldendpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
